387b479531a38f91d7b3620ffc63bba8936cfba4
Alex Johnson <johnsonav1992@gmail.com>
2023-08-27 13:39:02 -0500

    cards can flip!

--- src/components/MemoryCard/MemoryCard.tsx
+++ src/components/MemoryCard/MemoryCard.tsx
@@ -1,5 +1,9 @@
-import { Card } from '@mui/joy';
+import {
+    Box
+    , Card
+} from '@mui/joy';
 import Image from '../Image/Image';
+import { useState } from 'react';
 
 interface Props {
     width: string;
@@ -10,19 +14,44 @@ const MemoryCard = ( {
     width
     , image
 }: Props ) => {
-    console.log( image );
+    const [ isCardImageVisible, setIsCardImageVisible ] = useState( false );
+
+    const handleFlipCard = () => {
+        setIsCardImageVisible( currentVis => !currentVis );
+    };
+
     return (
         <Card
             sx={ {
                 width
                 , height: width
+                , p: !isCardImageVisible ? '.2rem' : 'inherit'
+                , '&:hover': {
+                    cursor: 'pointer'
+                }
             } }
+            onClick={ handleFlipCard }
         >
-            <Image
-                src={ image }
-                width='100%'
-                height='100%'
-            />
+            {
+                isCardImageVisible
+                    ? (
+                        <Image
+                            src={ image }
+                            width='100%'
+                            height='100%'
+                        />
+                    )
+                    : (
+                        <Box
+                            width='100%'
+                            height='100%'
+                            sx={ {
+                                backgroundColor: theme => theme.palette.primary[ 300 ]
+                                , borderRadius: 'inherit'
+                            } }
+                        />
+                    )
+            }
         </Card>
     );
 };
