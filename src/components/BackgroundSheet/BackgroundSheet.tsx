5a14d408ec78586032987dd93b285370dd2faa70
Alex Johnson <johnsonav1992@gmail.com>
2023-08-29 06:28:52 -0500

    revert height

--- src/components/BackgroundSheet/BackgroundSheet.tsx
+++ src/components/BackgroundSheet/BackgroundSheet.tsx
@@ -9,7 +9,6 @@ const BackgroundSheet = ( { children }: PropsWithChildren<unknown> ) => {
             variant='soft'
             sx={ {
                 width: '55vw'
-                , height: '55vw'
                 , p: '1rem'
                 , borderRadius: '1rem'
                 , boxShadow: theme => theme.shadow.xl
