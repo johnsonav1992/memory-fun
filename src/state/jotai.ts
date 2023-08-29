3f248782ad7f8f29fdba8a3c3a180808cdba649d
Alex Johnson <johnsonav1992@gmail.com>
2023-08-29 08:56:43 -0500

    flip state lines

--- src/state/jotai.ts
+++ src/state/jotai.ts
@@ -5,8 +5,8 @@ import { spaceCards } from '../utils/spaceCards';
 export const currentScreenAtom = atom<TMemoryGameState['currentScreen']>( 'start-screen' );
 export const currentDeckAtom = atom<TMemoryGameState['currentDeck']>( spaceCards );
 export const flippedCardsAtom = atom<TMemoryGameState['flippedCards']>( [] );
+export const currentPlayerAtom = atom<TMemoryGameState['currentPlayer']>( 'player1' );
 export const scoresAtom = atom<TMemoryGameState['scores']>( {
     player1: 0
     , player2: 0
 } );
-export const currentPlayerAtom = atom<TMemoryGameState['currentPlayer']>( 'player1' );
