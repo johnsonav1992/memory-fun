07a59810b595280158e0eb0ba052b5c480b484ea
Alex Johnson <johnsonav1992@gmail.com>
2023-08-29 20:21:57 -0500

    added player number amount

--- src/state/jotai.ts
+++ src/state/jotai.ts
@@ -9,6 +9,7 @@ import { TMemoryGameState } from '../types/types';
 import { spaceCards } from '../utils/spaceCards';
 
 export const currentScreenAtom = atom<TMemoryGameState['currentScreen']>( 'start-screen' );
+export const gamePlayersNumberAtom = atomWithReset<TMemoryGameState['gamePlayersNumber']>( 2 );
 export const currentDeckAtom = atom<TMemoryGameState['currentDeck']>( spaceCards );
 export const flippedCardsAtom = atomWithReset<TMemoryGameState['flippedCards']>( [] );
 export const currentPlayerAtom = atomWithReset<TMemoryGameState['currentPlayer']>( 'player1' );
