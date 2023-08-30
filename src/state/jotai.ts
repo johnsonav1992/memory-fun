dd4a97098ea57b6d6a462aa2343369d6e57183a7
Alex Johnson <johnsonav1992@gmail.com>
2023-08-29 20:19:42 -0500

    added resets

--- src/state/jotai.ts
+++ src/state/jotai.ts
@@ -1,5 +1,6 @@
 // Libraries
 import { atom } from 'jotai';
+import { atomWithReset } from 'jotai/utils';
 
 // Types
 import { TMemoryGameState } from '../types/types';
@@ -9,9 +10,9 @@ import { spaceCards } from '../utils/spaceCards';
 
 export const currentScreenAtom = atom<TMemoryGameState['currentScreen']>( 'start-screen' );
 export const currentDeckAtom = atom<TMemoryGameState['currentDeck']>( spaceCards );
-export const flippedCardsAtom = atom<TMemoryGameState['flippedCards']>( [] );
-export const currentPlayerAtom = atom<TMemoryGameState['currentPlayer']>( 'player1' );
-export const scoresAtom = atom<TMemoryGameState['scores']>( {
+export const flippedCardsAtom = atomWithReset<TMemoryGameState['flippedCards']>( [] );
+export const currentPlayerAtom = atomWithReset<TMemoryGameState['currentPlayer']>( 'player1' );
+export const scoresAtom = atomWithReset<TMemoryGameState['scores']>( {
     player1: 0
     , player2: 0
 } );
