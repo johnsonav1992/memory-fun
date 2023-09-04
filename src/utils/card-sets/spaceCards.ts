089fb48175a809f32808b601538c2634e390c77c
Alex Johnson <johnsonav1992@gmail.com>
2023-09-03 20:03:07 -0500

    move spacecards to s3

--- src/utils/card-sets/spaceCards.ts
+++ src/utils/card-sets/spaceCards.ts
@@ -7,76 +7,64 @@ import {
     , TMemoryCard
 } from '../../types/types';
 
-// Assets
-import astro1 from '../../assets/astro1.png';
-import astro2 from '../../assets/astro2.png';
-import earth from '../../assets/earth.png';
-import planet1 from '../../assets/planet1.png';
-import planet2 from '../../assets/planet2.png';
-import rocket1 from '../../assets/rocket1.png';
-import shuttle from '../../assets/shuttle.png';
-import star from '../../assets/star.png';
-import ufo from '../../assets/ufo.png';
-import alien from '../../assets/alien.png';
-
 export const spaceCards: ArrayOfLength<TMemoryCard, 10> = [
     {
         id: uuidv4()
-        , path: astro1
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/astro1.png'
         , name: 'astro1'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: astro2
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/astro2.png'
         , name: 'astro2'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: earth
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/earth.png'
         , name: 'earth'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: planet1
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/planet1.png'
         , name: 'planet1'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: planet2
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/planet2.png'
         , name: 'planet2'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: rocket1
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/rocket1.png'
         , name: 'rocket1'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: shuttle
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/shuttle1.png'
         , name: 'shuttle'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: star
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/star.png'
         , name: 'star'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: ufo
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/ufo.png'
         , name: 'ufo'
         , cardStatus: 'in-progress'
     }
     , {
         id: uuidv4()
-        , path: alien
+        , path: 'https://memory-game-imgs.s3.us-east-2.amazonaws.com/alien.png'
         , name: 'alien'
         , cardStatus: 'in-progress'
     }
